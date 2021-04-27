ac33a6526212f3701d3c5e896bb4dfe7bcc10136
Joshua <cookhjosh@ecs.vuw.ac.nz>
2021-04-27 22:43:07 +1200

    Added JSONLayout and populated it

---
+++ src/main/java/nz/ac/wgtn/swen301/assignment2/JSONLayout.java
@@ -0,0 +1,47 @@
+package nz.ac.wgtn.swen301.assignment2;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.log4j.Layout;
+import org.apache.log4j.spi.LoggingEvent;
+
+import java.io.IOException;
+
+public class JSONLayout extends Layout {
+
+
+  @Override
+  public String format(LoggingEvent loggingEvent) {
+
+    String format = "";
+
+    ObjectMapper objM = new ObjectMapper();
+    ObjectNode objN = objM.createObjectNode();
+    objN.put("logger", loggingEvent.getLoggerName());
+    objN.put("level", loggingEvent.getLevel().toString());
+    objN.put("starttime", loggingEvent.getTimeStamp());
+    objN.put("thread", loggingEvent.getThreadName());
+    objN.put("message", loggingEvent.getMessage().toString());
+    try {
+      format = objM.writerWithDefaultPrettyPrinter().writeValueAsString(objN);
+    }
+    catch(IOException e) {return null;}
+
+
+    return format;
+  }
+
+  @Override
+  public boolean ignoresThrowable() {
+    return false;
+  }
+
+  @Override
+  public void activateOptions() {
+
+  }
+
+
+}
+
