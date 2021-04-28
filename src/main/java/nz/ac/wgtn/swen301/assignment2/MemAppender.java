5e21029bf537148c39b57eaf04966c9b3887d7c3
Joshua <cookhjosh@ecs.vuw.ac.nz>
2021-04-28 23:46:06 +1200

    Added MemAppender.java

---
+++ src/main/java/nz/ac/wgtn/swen301/assignment2/MemAppender.java
@@ -0,0 +1,15 @@
+package nz.ac.wgtn.swen301.assignment2;
+
+import java.util.ArrayList;
+
+public class MemAppender {
+
+  ArrayList logEvents = new ArrayList();
+  String name;
+
+  public void getCurrentLogs(){
+
+
+  }
+
+}
