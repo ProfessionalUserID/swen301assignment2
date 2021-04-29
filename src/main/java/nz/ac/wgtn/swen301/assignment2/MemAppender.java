85b8a29f693a7a99efaff0090fd04138c7fd0124
Joshua <cookhjosh@ecs.vuw.ac.nz>
2021-04-29 17:50:14 +1200

    Task 4 complete

--- src/main/java/nz/ac/wgtn/swen301/assignment2/MemAppender.java
+++ src/main/java/nz/ac/wgtn/swen301/assignment2/MemAppender.java
@@ -1,22 +1,72 @@
 package nz.ac.wgtn.swen301.assignment2;
 
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.spi.LoggingEvent;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MemAppender {
+public class MemAppender extends AppenderSkeleton {
 
-  List<String> logEvents = new ArrayList();
+  List<LoggingEvent> logEvents = new ArrayList();
   private String name = "";
+  private long discardCount = 0;
+  private long maxSize = 1000;
 
-  public String getName(){
+  @Override
+  protected void append(LoggingEvent loggingEvent) {
+    logEvents.add(loggingEvent);
+
+    while (logEvents.size() > maxSize){
+      logEvents.remove(0);
+      discardCount++;
+    }
+
+  }
+
+  @Override
+  public void close() {
+
+  }
+
+  public String getMemName(){
     return name;
   }
 
-  public void setName(String name){
+  public void setMemName(String name){
     this.name = name;
   }
 
-  public List<String> getCurrentLogs(){
+  @Override
+  public boolean requiresLayout() {
+    return false;
+  }
+
+  public List<LoggingEvent> getCurrentLogs(){
+    return new ArrayList<>(logEvents);
+  }
+
+  public long getDiscardedLogCount(){
+    return discardCount;
+  }
+
+  public void exportToJSON(String fileName){
+    JSONLayout l = new JSONLayout();
+    StringBuilder s = new StringBuilder();
+
+    for (LoggingEvent x : logEvents){
+      s.append(l.format(x));
+    }
+
+    try {
+      BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+      writer.write(s.toString());
+      writer.close();
+    } catch (Exception ignored) {}
+
 
   }
+
 }
