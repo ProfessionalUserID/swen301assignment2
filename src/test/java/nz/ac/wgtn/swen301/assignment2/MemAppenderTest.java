9ca9e2c71b30b7c7846e366ecf1750afac84db37
Joshua <cookhjosh@ecs.vuw.ac.nz>
2021-05-01 00:46:57 +1200

    Added tests for MemAppender

--- src/test/java/nz/ac/wgtn/swen301/assignment2/MemAppenderTest.java
+++ src/test/java/nz/ac/wgtn/swen301/assignment2/MemAppenderTest.java
@@ -1,6 +1,37 @@
 package nz.ac.wgtn.swen301.assignment2;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggingEvent;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 public class MemAppenderTest {
 
+  @Test
+  public void test_MemAppender1(){
+    Logger l = Logger.getLogger(JSONLayout.class.toString());
+    MemAppender m = new MemAppender();
+    l.addAppender(m);
+
+    l.info("This is a test");
+    assertEquals(m.getCurrentLogs().size(), 1);
+  }
+
+  @Test
+  public void test_MemAppender2(){
+    Logger l = Logger.getLogger(JSONLayout.class.toString());
+    MemAppender m = new MemAppender();
+    l.addAppender(m);
+
+    for (int i = 0; i < 1050; i++){
+      l.warn("test2");
+    }
+    long x = m.getDiscardedLogCount();
+    assertEquals(x, 50);
+  }
 
 }
