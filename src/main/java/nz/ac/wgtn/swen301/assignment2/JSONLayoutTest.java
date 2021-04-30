95770fa068103f4aa8d6d52776cbedd6d47f6531
Joshua <cookhjosh@ecs.vuw.ac.nz>
2021-04-30 15:49:34 +1200

    Test 3 implemented

--- src/main/java/nz/ac/wgtn/swen301/assignment2/JSONLayoutTest.java
+++ src/main/java/nz/ac/wgtn/swen301/assignment2/JSONLayoutTest.java
@@ -81,4 +81,38 @@ public class JSONLayoutTest {
 
   }
 
+  @Test
+  public void test_JSONLayout3(){
+    Logger logger = Logger.getLogger(JSONLayout.class.toString());
+    long startTime = System.currentTimeMillis();
+    String message = "JSONLayout test three";
+
+    LoggingEvent event = new LoggingEvent(this.getClass().toString(),
+        logger,
+        startTime,
+        Level.ALL,
+        message,
+        Thread.currentThread().getName(),
+        null,null,null,null);
+
+
+    JSONLayout l = new JSONLayout();
+    ObjectMapper objM = new ObjectMapper();
+    JsonNode node = null;
+    try {
+      node = objM.readTree(l.format(event));
+    } catch (JsonProcessingException e){
+      e.printStackTrace();
+      fail("Test failed");
+    }
+
+    assertEquals(node.get("logger").toString(), "\"class nz.ac.wgtn.swen301.assignment2.JSONLayout\"");
+    assertEquals(node.get("level").toString(), "\"ALL\"");
+    assertEquals(node.get("starttime").toString(), "\""+startTime+"\"");
+    assertEquals(node.get("thread").toString(), "\"main\"");
+    assertEquals(node.get("message").toString(), "\"JSONLayout test three\"");
+
+
+  }
+
 }
